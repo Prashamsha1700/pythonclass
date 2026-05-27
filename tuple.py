b4b88c852572140bf8cf93a4674a77454dd37fe9
Prashamsha Baral <baralprashamsha4@gmail.com>
2026-05-27 08:01:55 +0545

    fisrt initial

--- tuple.py
+++ tuple.py
@@ -1,4 +1,15 @@
-#to convert list into tuple
-my_list =[1, 2, 3, 4, 5]
+# to convert list into tuples
+my_list = [1, 2, 3, 4, 5]
 my_tuple = tuple(my_list)
 print(my_tuple)
+
+# to convert string into tuple
+my_string ="Hello"
+my_tuple = tuple(my_string)
+print(my_tuple)
+
+# to convert tuple into string
+my_tuple = ('H', 'e', 'l', 'l', 'o')
+
+my_string =''.join(my_tuple)
+print(my_string)
