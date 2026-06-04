9cd6815029f7c6c30779c34d1076ccf991fb55d5
Prashamsha Baral <baralprashamsha4@gmail.com>
2026-06-04 08:33:23 +0545

    sync

--- class.py
+++ class.py
@@ -1,16 +1,23 @@
-class student:
-    def __init__(self,name,address):
-        self.name=name
-        self.address=address
+class Student:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
 
     def full_name(self):
         return f"{self.name} {self.address}"
 
 
-s =student("paru","pokhara")
+s = Student("paru", "pokhara")
 print(s.full_name())
 
+# Inheritance
+class School(Student):
+    def __init__(self, name, address, age):
+        super().__init__(name, address)
+        self.age = age
 
 
+my_school = School("paru", "pokhara", 18)
 
-
+print(my_school.full_name())
+print(my_school.age)
