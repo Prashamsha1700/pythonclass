4e4b16000ecca75dc54ca0804d4a3d5de93bac20
Prashamsha Baral <baralprashamsha4@gmail.com>
2026-06-04 08:13:52 +0545

    sync

--- class.py
+++ class.py
@@ -2,7 +2,15 @@ class student:
     def __init__(self,name,address):
         self.name=name
         self.address=address
-my_student=student("paru","pokhara")
-print(my_student.name)
-print(my_student.address)
+
+    def full_name(self):
+        return f"{self.name} {self.address}"
+
+
+s =student("paru","pokhara")
+print(s.full_name())
+
+
+
+
 
